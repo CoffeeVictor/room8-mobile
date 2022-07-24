e80c370925f632c49193b04ae2222cf7b8efd129
CoffeeVictor <victor.jgfsilva@gmail.com>
2022-07-24 05:57:25 -0300

    Corrects firebase auth

--- src/screens/AuthScreen/index.tsx
+++ src/screens/AuthScreen/index.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react"
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
-import { colors } from "../../constants/Colors"
-import { TextInput as IconTextInput } from "react-native-paper"
-import { Entypo } from "@expo/vector-icons"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { auth } from '../../config/firebase';
+import { colors } from "../../constants/Colors";
 
 export const AuthScreen: React.FC = () => {
 
-    const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onRegister = async () => {
+        try {
+            const credentials = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            )
+
+            console.log('Credentials:', credentials)
+        }
+        catch(e) {
+            console.error(e)
+        }
+    }
+
+    function onLogin() {
+        console.log('Login')
+    }
 
     return (
         <KeyboardAvoidingView
@@ -14,21 +35,31 @@ export const AuthScreen: React.FC = () => {
             // behavior={'padding'}
         >
             <View style={styles.formContainer}>
-                <TextInput style={styles.formInput} placeholder={'Type your email'} keyboardType={'email-address'} />
-                <IconTextInput
+                <TextInput
+                    style={styles.formInput}
+                    placeholder={'Type your email'}
+                    keyboardType={'email-address'}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize={'none'}
+                />
+                <TextInput
                     style={styles.formInput}
                     placeholder={'Type your password'}
                     secureTextEntry={!isPasswordVisible}
+                    value={password}
+                    onChangeText={setPassword}
+                    autoCapitalize={'none'}
                 />
             </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.loginButton}>
-                    <Text>
+                <TouchableOpacity style={styles.loginButton} onPress={onLogin} >
+                    <Text style={styles.loginText}>
                         Login
                     </Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.registerButton}>
-                    <Text>
+                <TouchableOpacity style={styles.registerButton} onPress={onRegister} >
+                    <Text style={styles.registerText}>
                         Register
                     </Text>
                 </TouchableOpacity>
@@ -64,7 +95,12 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 15,
         borderRadius: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    loginText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '700'
     },
     registerButton: {
         backgroundColor: 'white',
@@ -74,6 +110,11 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 15,
         borderRadius: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    registerText: {
+        color: colors.primary,
+        fontSize: 16,
+        fontWeight: '700'
     }
 })
