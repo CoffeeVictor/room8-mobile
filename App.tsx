518d1c28313c846ce5f75e38122c1957e2b7905c
Victor <jvgfs@cesar.org.br>
2022-07-26 17:07:19 -0300

    Fixes StatusBar styling

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ export default function App() {
         <AuthProvider>
           <Navigation />
         </AuthProvider>
-        <StatusBar />
+        <StatusBar style='inverted' />
       </SafeAreaProvider>
     );
   }
