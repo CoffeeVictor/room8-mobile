7765c89c71c40a321b508169d708aaa7de632c56
Maely-Notebook <maelyalways@gmail.com>
2022-08-10 22:14:37 -0300

    home page

--- src/screens/Home/HomeList/index.tsx
+++ src/screens/Home/HomeList/index.tsx
@@ -2,24 +2,56 @@ import { AntDesign } from '@expo/vector-icons';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView, StyleSheet } from 'react-native';
 import { colors } from '../../../constants/Colors';
+import { DataTable } from 'react-native-paper';
 
 export const HomeList: React.FC = ({ people }) => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        {people?.map((item) => (
-          <View key={item.name} style={styles.card}>
-            <Text style={styles.text}>{item.name}</Text>
-            <Text style={styles.text}> {item.status}</Text>
-            <Text style={styles.text}> R$ {item.totalexpense}</Text>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => console.log('delete')}
-            >
-              <AntDesign name='delete' size={20} color={colors.primary} />
-            </TouchableOpacity>
-          </View>
-        ))}
+        <View style={styles.containerTable}>
+          <DataTable>
+            <DataTable.Header style={styles.textTable}>
+              <DataTable.Title>
+                <Text style={styles.textTable}>Nome</Text>
+              </DataTable.Title>
+              <DataTable.Title>
+                <Text style={styles.textTable}>Status</Text>
+              </DataTable.Title>
+              <DataTable.Title numeric>
+                <Text style={styles.textTable}>R$</Text>
+              </DataTable.Title>
+              <DataTable.Title>-</DataTable.Title>
+            </DataTable.Header>
+            {people?.map((item) => (
+              <DataTable.Row style={styles.text} key={item.name + 'row'}>
+                <DataTable.Cell>
+                  <Text style={styles.text}>{item.name}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell>
+                  <Text style={styles.text}> {item.status}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell numeric>
+                  <Text style={styles.text}> {item.totalexpense}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell>
+                  {
+                    <TouchableOpacity
+                      style={styles.buttonRemove}
+                      onPress={() => console.log('código')}
+                    >
+                      <AntDesign
+                        name='delete'
+                        size={20}
+                        color={colors.primary}
+                      ></AntDesign>
+                    </TouchableOpacity>
+                  }
+                </DataTable.Cell>
+              </DataTable.Row>
+            ))}
+          </DataTable>
+        </View>
+
         <TouchableOpacity
           style={styles.buttonAdd}
           onPress={() => console.log('código')}
@@ -31,15 +63,22 @@ export const HomeList: React.FC = ({ people }) => {
   );
 };
 const styles = StyleSheet.create({
+  containerTable: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    width: '90%',
   },
 
   scrollView: {
     marginBottom: 10,
-    marginTop: 10,
+
     width: '100%',
     height: '100%',
   },
@@ -56,8 +95,13 @@ const styles = StyleSheet.create({
 
   text: {
     fontSize: 16,
-    color: colors.primary,
+    color: colors.heading,
     marginLeft: 5,
+  },
+  textTable: {
+    fontSize: 18,
+    backgroundColor: colors.accent,
+    color: colors.primary,
   },
   button: { paddingLeft: 40 },
   textButton: {
@@ -73,5 +117,11 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     alignItems: 'center',
     backgroundColor: colors.primary,
+  },
+  buttonRemove: {
+    width: '30%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
   },
 });
